f204b9ce44d57293fbe262fb400e2c46c8c899d2
liljemery <jeremyinoa67@gmail.com>
2024-08-17 14:09:58 -0400

    fix: responsiveness improvements

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
 
   return (
-    <section className="h-screen bg-[#383548]">
+    <section className="h-screen bg-[#383548] min-w-[30rem] max-w-[90rem] mx-auto">
       <Nav/>
       <Main/>
     </section>
